1f278f6bea484459e18a8ccc6d81cb3469b3c622
MatheusHenrique805 <125106306+MatheusHenrique805@users.noreply.github.com>
2023-05-29 16:47:10 -0300

    Update TestesQ7.py
    Signed-off-by: MatheusHenrique805 <125106306+MatheusHenrique805@users.noreply.github.com>

--- Lista 5/TestesQ7.py
+++ Lista 5/TestesQ7.py
@@ -37,7 +37,7 @@ assert encontra_e_faltante([True, 4, 7, 5]) == Exception
 assert encontra_e_faltante([1, 3, 5]) == Exception
 assert encontra_e_faltante([1, 2, 3]) == Exception
 assert encontra_e_faltante([0, 2]) == Exception
-assert encontra_e_faltante([5, 4, 9, 6, 7, -1, 3, 2, 8])
+assert encontra_e_faltante([5, 4, 9, 6, 7, -1, 3, 2, 8]) == Exception
 assert encontra_e_faltante([4, 6, 8, 2]) == Exception
 assert encontra_e_faltante([4, 8, 3, 7, 2, 1, 5, 6]) == Exception
 assert encontra_e_faltante([4, 3, 1, 5]) == 2
