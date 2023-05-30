ff51a2442f0f71db08c5af04973068f7681e1e20
MatheusHenrique805 <125106306+MatheusHenrique805@users.noreply.github.com>
2023-05-29 21:18:56 -0300

    Update TestesQ1.py
    Signed-off-by: MatheusHenrique805 <125106306+MatheusHenrique805@users.noreply.github.com>

--- Lista 5/TestesQ1.py
+++ Lista 5/TestesQ1.py
@@ -1,19 +1,36 @@
 '''1. Crie uma função chamada "encontrar_primos_gemeos" que receba um número inteiro n como parâmetro e retorne uma lista contendo todos os pares de números primos
 gêmeos menores que n. Os números primos gêmeos são dois números primos consecutivos que diferem em 2.'''
+def retornar_primos(num):
+    divisores = []
+    for i in range(1, num + 1):
+        if 2 % i == 0:
+            divisores.append(i)
+        else:
+            continue
+    
+    return True if len(divisores) == 2 else False
+
+
 def encontrar_primos_gemeos(numero):
     if type(numero) != int or numero <= 3:
         return Exception
     else:
         lista_primos = []
-        for i in range(2, numero + 1):
-            if i % 1 == 0 and i % i == 0:
+        for i in range(1, numero + 1):
+            if retornar_primos(i) == True:
                 lista_primos.append(i)
             else:
                 continue
 
         lista_gemeos = []
-        for indice in range()
-        
+        for indice in range(len(lista_primos) - 1):
+            if lista_primos[indice + 1] - lista_primos[indice] == 2:
+                lists_gemeos.append(lista_primos[indice], lista_primos[indice + 1])
+            else:
+                continue
+    
+    return lista_gemeos
+
     
 assert encontrar_primos_gemeos(0) == Exception
 assert encontrar_primos_gemeos(12.4) == Exception
