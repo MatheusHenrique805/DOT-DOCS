3552e525e6d4e257f0a370c152507ea1b29c01d1
MatheusHenrique805 <125106306+MatheusHenrique805@users.noreply.github.com>
2023-05-30 16:00:06 -0300

    Update TestesQ7.py
    Signed-off-by: MatheusHenrique805 <125106306+MatheusHenrique805@users.noreply.github.com>

--- Lista 5/TestesQ7.py
+++ Lista 5/TestesQ7.py
@@ -32,8 +32,37 @@ assert encontra_e_faltante(True) == Exception
 
 assert encontra_e_faltante(12) == Exception
 assert encontra_e_faltante('True') == Exception
-assert encontra_e_faltante(True) == Exception
+assert encontra_e_faltante(True) == Exception'''5. Escreva uma função chamada "verificar_ano_bissexto" que receba um número inteiro como parâmetro e retorne True se o ano for bissexto, e False caso contrário. Um ano é
+bissexto se for divisível por 4, mas não divisível por 100, exceto se for divisível por 400.'''
+def verificar_ano_bissexto(ano):
+    if type(ano) != int or ano == 0:
+        return Exception
+    else:
+        if ano % 4 == 0 and ano % 100 != 0:
+            return True
+        elif ano % 4 == 0 and ano % 100 == 0 and ano % 400 == 0:
+            return True
+        elif ano % 4 != 0:
+            return False
+        elif ano % 4 == 0 and ano % 100 == 0 and ano % 400 != 0:
+            return False
+        else:
+            return False
+
+
+
+assert verificar_ano_bissexto('True') == Exception
+assert verificar_ano_bissexto(True) == Exception
+assert verificar_ano_bissexto(12.32) == Exception
+assert verificar_ano_bissexto({}) == Exception
+assert verificar_ano_bissexto(()) == Exception
+assert verificar_ano_bissexto([]) == Exception
+assert verificar_ano_bissexto(0) == Exception
+assert verificar_ano_bissexto(2022)
+
 assert encontra_e_faltante(12.32) == Exception
+assert encontrar_e_faltante({}) == Exception
+assert encontrar_e_faltante(()) == Exception
 assert encontra_e_faltante([]) == Exception
 assert encontra_e_faltante(['2313', 3547]) == Exception
 assert encontra_e_faltante([12.4, 457, 143]) == Exception
