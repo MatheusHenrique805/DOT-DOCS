272c8e36141cfea64df2bdbb5722535246e125e0
MatheusHenrique805 <125106306+MatheusHenrique805@users.noreply.github.com>
2023-05-30 22:20:10 -0300

    Update TestesQ5.py
    Signed-off-by: MatheusHenrique805 <125106306+MatheusHenrique805@users.noreply.github.com>

--- Lista 5/TestesQ5.py
+++ Lista 5/TestesQ5.py
@@ -1,7 +1,5 @@
-'''5. Escreva uma função chamada "verificar_ano_bissexto" que receba um número inteiro como parâmetro e retorne True se o ano for bissexto, e False caso contrário. Um ano é
-bissexto se for divisível por 4, mas não divisível por 100, exceto se for divisível por 400.'''
 def verificar_ano_bissexto(ano):
-    if type(ano) != int or ano == 0:
+    if (type(ano) != int) or (ano <= 3):
         return Exception
     else:
         if ano % 4 == 0 and ano % 100 != 0:
@@ -16,7 +14,6 @@ def verificar_ano_bissexto(ano):
             return False
 
 
-
 assert verificar_ano_bissexto('True') == Exception
 assert verificar_ano_bissexto(True) == Exception
 assert verificar_ano_bissexto(12.32) == Exception
@@ -24,4 +21,15 @@ assert verificar_ano_bissexto({}) == Exception
 assert verificar_ano_bissexto(()) == Exception
 assert verificar_ano_bissexto([]) == Exception
 assert verificar_ano_bissexto(0) == Exception
-assert verificar_ano_bissexto(2022)
+assert verificar_ano_bissexto(3) == Exception
+assert verificar_ano_bissexto(4) == True
+assert verificar_ano_bissexto(399) == False
+assert verificar_ano_bissexto(400) == True
+assert verificar_ano_bissexto(2022) == False
+assert verificar_ano_bissexto(1004) == True
+assert verificar_ano_bissexto(1005) == False
+assert verificar_ano_bissexto(1000) == False
+assert verificar_ano_bissexto(2024) == True
+assert verificar_ano_bissexto(2032) == True
+assert verificar_ano_bissexto(4000) == True
+print('Testes OK!!!')
